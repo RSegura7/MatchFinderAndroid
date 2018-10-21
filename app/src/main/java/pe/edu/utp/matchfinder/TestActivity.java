1ee2e4d5010839bd89e67863e4d6d35df52b9f23
Andre2SM <sm.aandree@gmail.com>
2018-10-21 16:19:58 -0500

    Test

--- app/src/main/java/pe/edu/utp/matchfinder/TestActivity.java
+++ app/src/main/java/pe/edu/utp/matchfinder/TestActivity.java
@@ -2,6 +2,7 @@ package pe.edu.utp.matchfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Toast.makeText(TestActivity.this, "hola mundo", Toast.LENGTH_LONG);
     }
 }
