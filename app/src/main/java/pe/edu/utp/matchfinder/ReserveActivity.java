4721fd74c8d307e0156cb533b7e10b0785caeb91
Iosef <iosef.ayalahuaringa@gmail.com>
2018-11-17 10:53:19 -0500

    Ultimas actualizaciones

--- app/src/main/java/pe/edu/utp/matchfinder/ReserveActivity.java
+++ app/src/main/java/pe/edu/utp/matchfinder/ReserveActivity.java
@@ -1,13 +1,54 @@
 package pe.edu.utp.matchfinder;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class ReserveActivity extends AppCompatActivity {
-
+    Button bfecha, bhora;
+    EditText efecha, ehora;
+    private int dia,mes,ano,hora,minutos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve);
+
+        bfecha = (Button)findViewById(R.id.btn_fecha);
+        efecha = (EditText)findViewById(R.id.label_fecha);
+
+        bhora = (Button)findViewById(R.id.btn_hora);
+        ehora = (EditText)findViewById(R.id.label_hora);
+
+        bfecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    final Calendar calendar = Calendar.getInstance();
+                    dia = calendar.get(Calendar.DAY_OF_MONTH);
+                    mes = calendar.get(Calendar.MONTH);
+                    ano = calendar.get(Calendar.YEAR);
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            efecha.setText(dayOfMonth + "/" + month + "/" + year);
+                        }
+                    });
+                    datePickerDialog.show();
+            }
+        });
+        bhora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v==bhora){
+
+                }
+            }
+        });
     }
 }
