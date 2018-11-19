a7afd909239e6cfac62a0415361c78ac054e386f
Andre2SM <sm.aandree@gmail.com>
2018-11-18 23:14:12 -0500

    added navigation to reservation form

--- app/src/main/java/pe/edu/utp/matchfinder/LanCenterActivity.java
+++ app/src/main/java/pe/edu/utp/matchfinder/LanCenterActivity.java
@@ -1,7 +1,10 @@
 package pe.edu.utp.matchfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LanCenterActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LanCenterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lan_center);
+
+        Button btnReserve = (Button) findViewById(R.id.bt_reserve);
+
+        btnReserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReserveActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
